1ea1aa80cdc0da89eaa59b1072fb3b51fb0e7dec
Duziem Ugoji <tickettoduzz@gmail.com>
2022-06-17 15:46:17 +0100

    commiting before updating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './contact/contact.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
