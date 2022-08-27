b2e83b0665c47486e3c4981913e3193785cab009
Duziem Ugoji <tickettoduzz@gmail.com>
2022-08-27 07:50:16 +0100

    hosted on firebase

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # MyBlog
 
 ![Angular](https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white)
-![Vercel](https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white)
+![SCULLY v2](https://img.shields.io/badge/-SCULLY%20v2-%231D854A?style=for-the-badge)
+![Firebase](https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.0.
 
-## Development server
+## Hosted
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+This blog site is hosted on firebase. Click [here](https://musings-nwamugo.web.app/) to see it.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+Run `npm run scully` to build the static files. The build artifacts will be stored in the `dist/static/` directory
+## Development server
+
+After building, run `npm run scully:serve` for a dev server that stays running. Navigate to `http://localhost:1668/`. Note: You have to do the build process again if you introduce new static files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
 ## Running unit tests
 
--- scully.my-blog.config.ts
+++ scully.my-blog.config.ts
@@ -3,8 +3,7 @@ import { ScullyConfig } from '@scullyio/scully';
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer'
 
-const chromium = require('chrome-aws-lambda');
-let configuration = {
+export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "my-blog",
   // add spsModulePath when using de Scully Platform Server,
@@ -16,45 +15,13 @@ let configuration = {
         folder: "./mdfiles"
       }
     },
+  },
+  puppeteerLaunchOptions: {
+    executablePath:
+      process.platform === 'win32'
+        ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+        : process.platform === 'linux'
+        ? '/usr/bin/google-chrome'
+        : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
   }
-}
-
-const setConfiguration = async function() {
-  const options = process.env['AWS_LAMBDA_FUNCTION_NAME']
-    ? {
-        args: chromium.args,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless
-      }
-    : {
-        args: [],
-        executablePath:
-          process.platform === 'win32'
-            ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-            : process.platform === 'linux'
-            ? '/usr/bin/google-chrome'
-            : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-      };
-
-  const configObj = {
-    projectRoot: "./src",
-    projectName: "my-blog",
-    // add spsModulePath when using de Scully Platform Server,
-    outDir: './dist/static',
-    routes: {
-      '/posts/:id': {
-        type: 'contentFolder',
-        id: {
-          folder: "./mdfiles"
-        }
-      },
-    },
-    puppeteerLaunchOptions: options
-  };
-
-  return configObj;
 };
-
-setConfiguration().then(result => configuration = result);
-
-export const config: ScullyConfig = configuration;
